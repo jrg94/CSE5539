f870e25549f393bdaae8636d7fb6fe8c88f89afe
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-02-11 18:56:52 -0500

    Toying with parsing

--- musiviz/music_file.py
+++ musiviz/music_file.py
@@ -1,11 +1,19 @@
 import struct
 
+MAX = 4
+
+
 class MusicFile:
     def __init__(self, path: str):
         self.path = path
 
     def decode(self):
         with open(self.path, "rb") as music_file:
-            while True:
-                data = music_file.read(7)
-                print(struct.unpack(">7B", data))
+            i = 0
+            while i < MAX:
+                size_raw = music_file.read(4)
+                size_decoded = struct.unpack(">i", size_raw)[0] - 4
+                print(size_decoded)
+                payload = music_file.read(size_decoded)
+                print(struct.unpack(">" + str(size_decoded) + "B", payload))
+                i += 1

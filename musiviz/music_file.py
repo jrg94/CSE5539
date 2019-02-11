17f076ca17128b520aee0bab69a4da026dddd41a
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-02-11 18:59:59 -0500

    Added unlimited reading

--- musiviz/music_file.py
+++ musiviz/music_file.py
@@ -9,11 +9,15 @@ class MusicFile:
 
     def decode(self):
         with open(self.path, "rb") as music_file:
-            i = 0
-            while i < MAX:
-                size_raw = music_file.read(4)
-                size_decoded = struct.unpack(">i", size_raw)[0] - 4
-                print(size_decoded)
-                payload = music_file.read(size_decoded)
-                print(struct.unpack(">" + str(size_decoded) + "B", payload))
-                i += 1
+            size = MusicFile.get_size(music_file)
+            while size != 0:
+                print(size)
+                payload = music_file.read(size)
+                print(struct.unpack(">" + str(size) + "B", payload))
+                size = MusicFile.get_size(music_file)
+
+    @staticmethod
+    def get_size(music_file):
+        size_raw = music_file.read(4)
+        size_decoded = struct.unpack(">i", size_raw)[0] - 4
+        return size_decoded

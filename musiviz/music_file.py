2015956ce02f57fd91b44ac0c08d43a7f7b8bcda
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-02-11 18:14:48 -0500

    Successfully read from file

--- musiviz/music_file.py
+++ musiviz/music_file.py
@@ -1,7 +1,11 @@
+import struct
+
 class MusicFile:
     def __init__(self, path: str):
         self.path = path
 
     def decode(self):
-        with open(self.path) as music_file:
-            print(music_file)
+        with open(self.path, "rb") as music_file:
+            while True:
+                data = music_file.read(7)
+                print(struct.unpack(">7B", data))

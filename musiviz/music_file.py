be7fa3107e84c40b326edefb3819fd2a04b462cf
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-02-12 13:36:45 -0500

    Fixed sub chunk reading

--- musiviz/music_file.py
+++ musiviz/music_file.py
@@ -3,7 +3,7 @@ import os
 import io
 import json
 
-WORD_SIZE = 4
+HEADER_SIZE = 8
 
 ATOMS = [
     "moov",
@@ -46,62 +46,72 @@ class MusicFile:
                 root_mapping[atom[1]]["children"] = chunk_mapping
                 MusicFile.traverse_atoms(chunks, chunk_mapping)
 
-
     @staticmethod
     def read_sub_chunks(chunk: tuple) -> list:
         byte_stream = io.BytesIO(chunk[2])
-        chunks = MusicFile.read_chunks(byte_stream, chunk[0])
+        chunks = MusicFile.read_chunks(byte_stream, chunk[0] - 8)
         return chunks
 
     @staticmethod
-    def read_chunks(stream, stream_size) -> list:
+    def read_chunks(stream: io.BytesIO, stream_size: int) -> list:
         read_stream_size = 0
         stream_chunks = list()
-        while read_stream_size + 8 < stream_size:
+        while read_stream_size < stream_size:
             chunk = MusicFile.read_chunk(stream)
             print(chunk[:-1])
+            print(len(chunk[-1]))
             stream_chunks.append(chunk)
             read_stream_size += chunk[0]
         return stream_chunks
 
     @staticmethod
-    def read_chunk(music_file) -> tuple:
+    def read_chunk(music_file: io.BytesIO) -> tuple:
         """
         Reads a chunk from an M4A file.
 
         :param music_file: an open music file
         :return: a tuple defining this chunk
         """
-        size = MusicFile._get_size(music_file)
-        data_type = MusicFile._get_type(music_file)
-        payload = MusicFile._get_payload(music_file, size - 8)
+        size, data_type = MusicFile._read_header(music_file)
+        payload = b''
+        if size != 0:
+            payload = MusicFile._get_payload(music_file, size - 8)
         return size, data_type, payload
 
     @staticmethod
-    def _get_size(music_file) -> int:
+    def _read_header(music_file: io.BytesIO):
+        header_raw = music_file.read(HEADER_SIZE)
+        size = MusicFile._get_size(header_raw[:4])
+        data_type = ""
+        if size != 0:
+            data_type = MusicFile._get_type(header_raw[4:])
+        return size, data_type
+
+    @staticmethod
+    def _get_size(header_size_raw: bytes) -> int:
         """
         Grabs the size of the current chunk from a file.
 
         :param music_file: an open music file
         :return: the size of the current chunk in bytes
         """
-        size_raw = music_file.read(WORD_SIZE)
-        size_decoded = struct.unpack(">i", size_raw)[0]
+        size_decoded = 0
+        if len(header_size_raw) != 0:
+            size_decoded = struct.unpack(">i", header_size_raw)[0]
         return size_decoded
 
     @staticmethod
-    def _get_type(music_file) -> str:
+    def _get_type(header_type_raw: bytes) -> str:
         """
         Grabs the type of payload from a file.
 
         :param music_file: an open music file
         :return: the type as a string
         """
-        data_type = music_file.read(4)
-        return data_type.decode("utf-8")
+        return header_type_raw.decode("utf-8")
 
     @staticmethod
-    def _get_payload(music_file, payload_size: int) -> bytes:
+    def _get_payload(music_file: io.BytesIO, payload_size: int) -> bytes:
         """
         Grabs and returns the next payload_size bytes from a file.
 

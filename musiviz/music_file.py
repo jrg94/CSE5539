689c741f58adbf230223caadd3558358e66ecf7b
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-02-11 20:25:55 -0500

    Readin works!

--- musiviz/music_file.py
+++ musiviz/music_file.py
@@ -9,14 +9,15 @@ class MusicFile:
 
     def decode(self):
         with open(self.path, "rb") as music_file:
-            size = MusicFile.get_size(music_file)
-            while size != 0:
-                print(size)
-                data_type = MusicFile.get_type(music_file)
-                print(str(data_type))
-                payload = music_file.read(size - 8)
-                size = MusicFile.get_size(music_file)
-                #print(struct.unpack(">" + str(size) + "s", payload))
+            while music_file.readable():
+                print(MusicFile.read_chunk(music_file)[:-1])
+
+    @staticmethod
+    def read_chunk(music_file) -> tuple:
+        size = MusicFile.get_size(music_file)
+        data_type = MusicFile.get_type(music_file)
+        payload = MusicFile.get_payload(music_file, size - 8)
+        return size, data_type, payload
 
     @staticmethod
     def get_size(music_file) -> int:
@@ -28,3 +29,8 @@ class MusicFile:
     def get_type(music_file) -> str:
         data_type = music_file.read(4)
         return data_type.decode("utf-8")
+
+    @staticmethod
+    def get_payload(music_file, payload_size: int):
+        payload = music_file.read(payload_size)
+        return payload

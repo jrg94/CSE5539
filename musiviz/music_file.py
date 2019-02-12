ca9d5221924ff4621aaf4c0701af1d28c3b0b633
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-02-11 21:48:09 -0500

    Documented all methods

--- musiviz/music_file.py
+++ musiviz/music_file.py
@@ -11,23 +11,44 @@ class MusicFile:
 
     def decode(self):
         with open(self.path, "rb") as music_file:
+            file_chunks = list()
             os_file_size = os.path.getsize(self.path)
             while self.read_file_size < os_file_size:
-                print(self.read_chunk(music_file)[:-1])
+                chunk = self.read_chunk(music_file)
+                print(chunk[:-1])
+                file_chunks.append(chunk)
 
     def read_chunk(self, music_file) -> tuple:
+        """
+        Reads a chunk from an M4A file.
+
+        :param music_file: an open music file
+        :return: a tuple defining this chunk
+        """
         size = self._get_size(music_file)
         data_type = self._get_type(music_file)
         payload = self._get_payload(music_file, size - 8)
         return size, data_type, payload
 
     def _get_size(self, music_file) -> int:
+        """
+        Grabs the size of the current chunk from a file.
+
+        :param music_file: an open music file
+        :return: the size of the current chunk in bytes
+        """
         self.read_file_size += WORD_SIZE
         size_raw = music_file.read(WORD_SIZE)
         size_decoded = struct.unpack(">i", size_raw)[0]
         return size_decoded
 
     def _get_type(self, music_file) -> str:
+        """
+        Grabs the type of payload from a file.
+
+        :param music_file: an open music file
+        :return: the type as a string
+        """
         self.read_file_size += WORD_SIZE
         data_type = music_file.read(4)
         return data_type.decode("utf-8")

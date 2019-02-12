6d01ae6840118aa9ce9be53b91b17a142f1f0566
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-02-11 19:53:44 -0500

    Improved reading cycle

--- musiviz/music_file.py
+++ musiviz/music_file.py
@@ -12,12 +12,19 @@ class MusicFile:
             size = MusicFile.get_size(music_file)
             while size != 0:
                 print(size)
-                payload = music_file.read(size)
-                print(struct.unpack(">" + str(size) + "B", payload))
+                data_type = MusicFile.get_type(music_file)
+                print(str(data_type))
+                payload = music_file.read(size - 8)
                 size = MusicFile.get_size(music_file)
+                #print(struct.unpack(">" + str(size) + "s", payload))
 
     @staticmethod
-    def get_size(music_file):
+    def get_size(music_file) -> int:
         size_raw = music_file.read(4)
-        size_decoded = struct.unpack(">i", size_raw)[0] - 4
+        size_decoded = struct.unpack(">i", size_raw)[0]
         return size_decoded
+
+    @staticmethod
+    def get_type(music_file) -> str:
+        data_type = music_file.read(4)
+        return data_type.decode("utf-8")

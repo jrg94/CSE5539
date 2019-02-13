f14d55726f69675f7e0456681d147f8958e3a956
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-02-13 14:59:32 -0500

    Successfully ran data collection on all aaron west songs

--- musiviz/music_file.py
+++ musiviz/music_file.py
@@ -9,11 +9,12 @@ class MusicFile:
 
     def __init__(self, path: str):
         self.path = path
+        self._raw_json = None
 
     def load(self):
-        file_dict = m4a_parse.decode(self.path)
+        self._raw_json = m4a_parse.decode(self.path)
         data_dir = "data\\" + "\\".join(self.path.split("\\")[-3:-1])
         json_name = self.path.split("\\")[-1].replace(".m4a", ".json")
         pathlib.Path(data_dir).mkdir(parents=True, exist_ok=True)
         with open(os.path.join(data_dir, json_name), "w") as f:
-            print(json.dumps(file_dict, indent=2, sort_keys=True, ensure_ascii=False), file=f)
+            print(json.dumps(self._raw_json, indent=2, sort_keys=True, ensure_ascii=False), file=f)

4b9423282a8d714f42246a2beb1514a973045e85
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-02-24 14:54:15 -0500

    Tried converting c code directly

--- midterm-project/signal_processing.m
+++ midterm-project/signal_processing.m
@@ -1,5 +1,5 @@
-K = 200; % Window width
-L = 125; % Longest lag
+MAX_WINDOW = 200; % K
+MAX_DELAY = 125; % L
 F0_MIN = 80; % Hz
 F0_MAX = 222; % Hz
 TIME_STEPS = 325;
@@ -8,4 +8,15 @@ CHANNELS = 64;
 ar0 = load("data/ar0.dat");
 ar0_grid = reshape(ar0, [325, 64]).';
 
-correlogram(ar0_grid, 325, L, K, 64)
+acg = zeros(MAX_DELAY, CHANNELS);
+for channel = 1:CHANNELS
+   for delay = 1:MAX_DELAY
+       for window = 1:MAX_WINDOW
+           acg(delay, channel) = acg(delay, channel) + (ar0_grid(channel, window) * ar0_grid(channel, window + delay));
+       end
+   end
+end
+
+plot(acg)
+
+% correlogram(ar0_grid, 325, L, K, 64)

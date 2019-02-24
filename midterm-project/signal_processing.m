93b3218ccf28362de72b91ede69d2f6f2530a71d
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-02-24 17:29:46 -0500

    I give up

--- midterm-project/signal_processing.m
+++ midterm-project/signal_processing.m
@@ -4,6 +4,7 @@ F0_MIN = 80; % Hz
 F0_MAX = 222; % Hz
 TIME_STEPS = 325;
 CHANNELS = 64;
+SAMPLING_FQ = 10000; % Hz
 LOWER_FQ = 80; % Hz
 UPPER_FQ = 4000; % Hz
 
@@ -15,10 +16,17 @@ summary = zeros(MAX_DELAY);
 for channel = 1:CHANNELS
    for delay = 1:MAX_DELAY
        for window = 1:MAX_WINDOW
+           % see: acg[delay][chan]+=getBufferVal(&cochlea[chan],win)*getBufferVal(&cochlea[chan],win+delay);
            acg(delay, channel) = acg(delay, channel) + (ar0_grid(channel, window) * ar0_grid(channel, window + delay));
        end
        summary(delay) = summary(delay) + acg(delay, channel);
    end
+end
+
+for channel = 1: CHANNELS
+    for delay = 1:MAX_DELAY
+        acg(delay, channel) = acg(delay, channel) / (MAX_WINDOW * 50);
+    end
 end
 
 plot(acg)

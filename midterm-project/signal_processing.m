4e72799387e462098daeab0464319ba217771dd4
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-02-24 11:55:31 -0500

    Wrote an indexing loop

--- midterm-project/signal_processing.m
+++ midterm-project/signal_processing.m
@@ -3,3 +3,13 @@ L = 125; % Longest lag
 F0_MIN = 80; % Hz
 F0_MAX = 222; % Hz
 TIME_STEPS = 325;
+
+ar0 = load("data/ar0.dat");
+
+% Running autocorrelation
+for i = 1:64
+    for j = 1:325
+        k = ((i - 1) * 325) + j;
+        ar0(k);
+    end
+end

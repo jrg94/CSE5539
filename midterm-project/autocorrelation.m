7d7396be14ac4d4d3ba11515b5ef0238ac0a1689
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-02-25 11:44:41 -0500

    I give up

--- midterm-project/autocorrelation.m
+++ midterm-project/autocorrelation.m
@@ -1,9 +1,9 @@
-function A = autocorrelation(data, i, j, K, L)
+function A = autocorrelation(data, i, j, t, K, SAMPLING_FQ)
     A = 0;
     for k = 0: K - 1
         r1 = data(i, j - k);
-        r2 = data(i, j - k - L);
-        w = k;
-        A = A + r1 * r2 * w;
+        r2 = data(i, j - k - t);
+        w = k / SAMPLING_FQ;
+        A = A + (r1 * r2 * w);
     end
 end

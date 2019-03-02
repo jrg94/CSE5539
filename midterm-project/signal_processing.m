3451c1411d0de2e6e323edff02ac218f4ed483ee
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-02 14:15:52 -0500

    Removed extraneous code

--- midterm-project/signal_processing.m
+++ midterm-project/signal_processing.m
@@ -2,11 +2,8 @@ MAX_WINDOW = 200; % K
 MAX_DELAY = 125; % L
 F0_MIN = 80; % Hz
 F0_MAX = 222; % Hz
-TIME_STEPS = 325;
 CHANNELS = 64;
 SAMPLING_FQ = 10000; % Hz
-LOWER_FQ = 80; % Hz
-UPPER_FQ = 4000; % Hz
 
 ar0 = load("data/ar0.dat");
 ar0_grid = reshape(ar0, [64, 325]); 

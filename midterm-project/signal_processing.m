f7933097a6f09c690288cb760b93ee863bddbddd
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-02 14:20:20 -0500

    Calculated frequency of both data files

--- midterm-project/signal_processing.m
+++ midterm-project/signal_processing.m
@@ -7,9 +7,10 @@ SAMPLING_FQ = 10000; % Hz
 
 ar0 = load("data/ar0.dat");
 ar0_grid = reshape(ar0, [64, 325]); 
-
-[acg, summary] = correlogram(ar0_grid, MAX_DELAY, CHANNELS, MAX_WINDOW);
+[~, summary] = correlogram(ar0_grid, MAX_DELAY, CHANNELS, MAX_WINDOW);
 f0_ar0 = fundamental_frequency(SAMPLING_FQ, F0_MIN, F0_MAX, summary);
 
-plot(summary)
-xlabel("Lag Index")
+er4 = load("data/er4.dat");
+er4_grid = reshape(er4, [64, 325]); 
+[~, summary] = correlogram(er4_grid, MAX_DELAY, CHANNELS, MAX_WINDOW);
+f0_er4 = fundamental_frequency(SAMPLING_FQ, F0_MIN, F0_MAX, summary);

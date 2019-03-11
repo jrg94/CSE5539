26ee6446fccb663437ab95d7366054a15f555a7f
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-11 15:00:34 -0400

    Created inefficient data processing code

--- term-project/musiparse/main.py
+++ term-project/musiparse/main.py
@@ -3,10 +3,16 @@ from musiparse.music_file_set import MusicFileSet
 SAMPLE_PATH = "E:\\Plex\\Music"
 
 
-def main():
+def genre_json_dump(limit: int, genre: str):
     music_set = MusicFileSet()
-    music_set.add_all(SAMPLE_PATH, limit=5, genre="Jazz")
-    music_set.to_json("..\\data\\jazz_5.json")
+    music_set.add_all(SAMPLE_PATH, limit=limit, genre=genre)
+    music_set.to_json("..\\data\\%s_%d.json" % (genre.lower(), limit))
+
+
+def main():
+    genre_json_dump(5, "Jazz")
+    genre_json_dump(5, "Rock")
+    genre_json_dump(5, "Alternative")
 
 
 if __name__ == '__main__':

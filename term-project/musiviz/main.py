72ee7c3e2566674487e15569e9dacacc06930d1c
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-11 14:10:34 -0400

    Added all file processing

--- term-project/musiviz/main.py
+++ term-project/musiviz/main.py
@@ -1,7 +1,7 @@
 from musiviz.music_file_set import MusicFileSet
 import os
 
-SAMPLE_PATH = "E:\\Plex\\Music\\Glenn Miller Orchestra"
+SAMPLE_PATH = "E:\\Plex\\Music"
 
 
 def main():
@@ -9,8 +9,12 @@ def main():
     for path, dirs, files in os.walk(SAMPLE_PATH):
         for file in files:
             if file.split(".")[-1] == "m4a":
-                music_set.add(os.path.join(path, file))
-    music_set.to_json("example.json")
+                print("Processing '%s'" % file)
+                try:
+                    music_set.add(os.path.join(path, file))
+                except:
+                    print("- Failed to process '%s'" % file)
+    music_set.to_json("data\\master.json")
 
 
 if __name__ == '__main__':

567cdf0923d07ce791a0ebe0ded5681b417f9a41
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-11 15:09:11 -0400

    Added two more genre filtered files

--- term-project/musiparse/main.py
+++ term-project/musiparse/main.py
@@ -3,6 +3,12 @@ from musiparse.music_file_set import MusicFileSet
 SAMPLE_PATH = "E:\\Plex\\Music"
 
 
+def master_json_dump(limit: int):
+    music_set = MusicFileSet()
+    music_set.add_all(SAMPLE_PATH, limit=limit)
+    music_set.to_json("..\\data\\master_%d.json" % limit)
+
+
 def genre_json_dump(limit: int, genre: str):
     music_set = MusicFileSet()
     music_set.add_all(SAMPLE_PATH, limit=limit, genre=genre)
@@ -10,9 +16,11 @@ def genre_json_dump(limit: int, genre: str):
 
 
 def main():
-    genre_json_dump(5, "Jazz")
-    genre_json_dump(5, "Rock")
-    genre_json_dump(5, "Alternative")
+    master_json_dump(100)
+    master_json_dump(500)
+    #genre_json_dump(5, "Jazz")
+    #genre_json_dump(5, "Rock")
+    #genre_json_dump(5, "Alternative")
 
 
 if __name__ == '__main__':

15e563b8af0086711b9c345d98f7617aabb721b3
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-11 22:18:17 -0400

    Added release date support

--- term-project/musiviz/graph.js
+++ term-project/musiviz/graph.js
@@ -10,4 +10,19 @@ var svg = d3.select("svg");
 
 d3.json("../data/master_5000.json").then(function(data) {
     console.log(data);
+
+    var xScale = d3.scaleLinear()
+		.domain([0, d3.max(data, function(d) { return timeToSeconds(d.length); })])
+		//.range([padding, w - padding * 2]);
+
+	var yScale = d3.scaleLinear()
+		.domain([0, d3.max(data, function(d) { return d.purchase_date; })])
+		//.range([h - padding, padding]);
 });
+
+function timeToSeconds(time) {
+    elements = time.split(":");
+    hoursToSeconds = elements[0] * 3600;
+    minutesToSeconds = elements[1] * 60;
+    return hoursToSeconds + minutesToSeconds + elements[2];
+}

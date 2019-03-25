5cfd68973f22a8ee58ab113d5c2e01fe4ada28f8
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-24 20:40:25 -0400

    Added y-axis

--- term-project/musiviz/graph.js
+++ term-project/musiviz/graph.js
@@ -131,20 +131,7 @@ function dBFSVsReleaseDate(data) {
     .style("text-anchor", "middle")
     .text("Release Date (Year)");
 
-  // Draw y-axis
-  svg.append("g")
-    .attr("transform", "translate(" + padding + ", 0)")
-    .call(d3.axisLeft(yScale));
-
-  // Draw y-axis title
-  svg.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - 5)
-    .attr("x", 0 - (height / 2))
-    .attr("dy", "1em")
-    .style("text-anchor", "middle")
-    .text("dBFS");
-
+  drawYAxis(yScale, padding, height, "dBFS");
   drawLegend(colorScale);
   drawTitle("dBFS vs. Release Date");
 }
@@ -361,6 +348,22 @@ function timeToSeconds(time) {
   hoursToSeconds = Number(elements[0]) * 3600;
   minutesToSeconds = Number(elements[1]) * 60;
   return hoursToSeconds + minutesToSeconds + Number(elements[2]);
+}
+
+function drawYAxis(yScale, padding, height, label) {
+  // Draw y-axis
+  svg.append("g")
+    .attr("transform", "translate(" + padding + ", 0)")
+    .call(d3.axisLeft(yScale));
+
+  // Draw y-axis title
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - 5)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text(label);
 }
 
 /**

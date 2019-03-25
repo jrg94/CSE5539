390acfab8af75e199a0d2e0be1a2accfc741181d
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2019-03-24 20:26:56 -0400

    Duplicated duration code

--- term-project/musiviz/graph.js
+++ term-project/musiviz/graph.js
@@ -10,64 +10,142 @@ var svg = d3.select("svg");
 
 function genreHistogram(data) {
 
-    var mapping = []
-    data.forEach(function(d) {
-        if (d.genre == null) {
-            d.genre = "None";
-        }
-        var found = false;
-        for (var i = 0; i < mapping.length && !found; i++) {
-            if (mapping[i].genre == d.genre) {
-                mapping[i].count += 1;
-                found = true;
-            }
-        }
-        if (!found) {
-            mapping.push({"genre": d.genre, "count": 1})
-        }
+  var mapping = []
+  data.forEach(function(d) {
+    if (d.genre == null) {
+      d.genre = "None";
+    }
+    var found = false;
+    for (var i = 0; i < mapping.length && !found; i++) {
+      if (mapping[i].genre == d.genre) {
+        mapping[i].count += 1;
+        found = true;
+      }
+    }
+    if (!found) {
+      mapping.push({
+        "genre": d.genre,
+        "count": 1
+      })
+    }
+  });
+  mapping.sort((a, b) => b.count - a.count)
+
+  var xScale = d3.scaleBand()
+    .domain(mapping.map(function(d) {
+      return d.genre;
+    }))
+    .range([padding, width - padding * 2])
+    .padding(.1);
+
+  var yScale = d3.scaleLinear()
+    .domain([0, d3.max(mapping, function(d) {
+      return d.count;
+    })])
+    .range([height - padding, padding]);
+
+  svg.append("g")
+    .attr("transform", "translate(0," + (height - padding) + ")")
+    .call(d3.axisBottom(xScale))
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", "rotate(-35)");
+
+  // Draw y-axis
+  svg.append("g")
+    .attr("transform", "translate(" + padding + ", 0)")
+    .call(d3.axisLeft(yScale));
+
+  svg.selectAll(".bar")
+    .data(mapping)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("fill", "#dc3912")
+    .attr("x", function(d) {
+      return xScale(d.genre);
+    })
+    .attr("y", function(d) {
+      return yScale(d.count);
+    })
+    .attr("width", xScale.bandwidth())
+    .attr("height", function(d) {
+      return height - yScale(d.count) - padding;
     });
-    mapping.sort((a, b) => b.count - a.count)
 
-    var xScale = d3.scaleBand()
-        .domain(mapping.map(function(d) { return d.genre; }))
-        .range([padding, width - padding * 2])
-        .padding(.1);
+  drawTitle("Genre Histogram");
+}
 
-    var yScale = d3.scaleLinear()
-        .domain([0, d3.max(mapping, function (d) { return d.count; })])
-        .range([height - padding, padding]);
+function dBFSVsReleaseData(data) {
+  // Filter out useless data
+  data = data.filter(filterReleaseDate)
 
-    svg.append("g")
-	    .attr("transform", "translate(0," + (height - padding) + ")")
-	    .call(d3.axisBottom(xScale))
-	    .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dx", "-.8em")
-            .attr("dy", ".15em")
-            .attr("transform", "rotate(-35)");
+  // Update purchase date from string to date object
+  data.forEach(function(d) {
+    d.release_date = new Date(d.release_date);
+  });
 
-	// Draw y-axis
-    svg.append("g")
-    	.attr("transform", "translate(" + padding + ", 0)")
-        .call(d3.axisLeft(yScale));
+  // Create x scale
+  var yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) {
+      return timeToSeconds(d.length);
+    })])
+    .range([height - padding, padding]);
 
-    svg.selectAll(".bar")
-        .data(mapping)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("fill", "#dc3912")
-        .attr("x", function (d) {
-            return xScale(d.genre);
-        })
-        .attr("y", function (d) {
-            return yScale(d.count);
-        })
-        .attr("width", xScale.bandwidth())
-        .attr("height", function (d) {
-            return height - yScale(d.count) - padding;
-        });
+  // Create y scale
+  var xScale = d3.scaleTime()
+    .domain(d3.extent(data, function(d) {
+      return d.release_date;
+    }))
+    .range([padding, width - padding * 2]);
 
-    drawTitle("Genre Histogram");
+  // Create color scale
+  var colorScale = d3.scaleOrdinal(d3.schemePaired);
+
+  // Draw all circles
+  svg.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cy", function(d) {
+      return yScale(timeToSeconds(d.length));
+    })
+    .attr("cx", function(d) {
+      return xScale(d.release_date);
+    })
+    .attr("r", 5)
+    .attr("fill", function(d) {
+      return colorScale(d.genre);
+    });
+
+  // Draw x-axis
+  svg.append("g")
+    .attr("transform", "translate(0," + (height - padding) + ")")
+    .call(d3.axisBottom(xScale));
+
+  // Draw x-axis title
+  svg.append("text")
+    .attr("transform", "translate(" + ((width / 2) - padding / 2) + " ," + (height - 10) + ")")
+    .style("text-anchor", "middle")
+    .text("Release Date (Year)");
+
+  // Draw y-axis
+  svg.append("g")
+    .attr("transform", "translate(" + padding + ", 0)")
+    .call(d3.axisLeft(yScale));
+
+  // Draw y-axis title
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - 5)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Duration (seconds)");
+
+  drawLegend(colorScale);
+  drawTitle("Duration vs. Release Date");
 }
 
 /**
@@ -76,68 +154,74 @@ function genreHistogram(data) {
  * @param {Object} data - a music object
  */
 function durationVsReleaseDate(data) {
-    // Filter out useless data
-    data = data.filter(filterReleaseDate)
+  // Filter out useless data
+  data = data.filter(filterReleaseDate)
 
-    // Update purchase date from string to date object
-    data.forEach(function(d) {
-        d.release_date = new Date(d.release_date);
+  // Update purchase date from string to date object
+  data.forEach(function(d) {
+    d.release_date = new Date(d.release_date);
+  });
+
+  // Create x scale
+  var yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) {
+      return timeToSeconds(d.length);
+    })])
+    .range([height - padding, padding]);
+
+  // Create y scale
+  var xScale = d3.scaleTime()
+    .domain(d3.extent(data, function(d) {
+      return d.release_date;
+    }))
+    .range([padding, width - padding * 2]);
+
+  // Create color scale
+  var colorScale = d3.scaleOrdinal(d3.schemePaired);
+
+  // Draw all circles
+  svg.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cy", function(d) {
+      return yScale(timeToSeconds(d.length));
+    })
+    .attr("cx", function(d) {
+      return xScale(d.release_date);
+    })
+    .attr("r", 5)
+    .attr("fill", function(d) {
+      return colorScale(d.genre);
     });
 
-    // Create x scale
-    var yScale = d3.scaleLinear()
-		.domain([0, d3.max(data, function(d) { return timeToSeconds(d.length); })])
-	    .range([height - padding, padding]);
+  // Draw x-axis
+  svg.append("g")
+    .attr("transform", "translate(0," + (height - padding) + ")")
+    .call(d3.axisBottom(xScale));
 
-    // Create y scale
-	var xScale = d3.scaleTime()
-		.domain(d3.extent(data, function(d) { return d.release_date; }))
-		.range([padding, width - padding * 2]);
+  // Draw x-axis title
+  svg.append("text")
+    .attr("transform", "translate(" + ((width / 2) - padding / 2) + " ," + (height - 10) + ")")
+    .style("text-anchor", "middle")
+    .text("Release Date (Year)");
 
-    // Create color scale
-	var colorScale = d3.scaleOrdinal(d3.schemePaired);
+  // Draw y-axis
+  svg.append("g")
+    .attr("transform", "translate(" + padding + ", 0)")
+    .call(d3.axisLeft(yScale));
 
-    // Draw all circles
-    svg.selectAll("circle")
-		.data(data)
-		.enter()
-		.append("circle")
-		.attr("cy", function(d) {
-			return yScale(timeToSeconds(d.length));
-		})
-		.attr("cx", function(d) {
-		    return xScale(d.release_date);
-		})
-		.attr("r", 5)
-		.attr("fill", function(d) { return colorScale(d.genre); } );
+  // Draw y-axis title
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - 5)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Duration (seconds)");
 
-    // Draw x-axis
-	svg.append("g")
-	    .attr("transform", "translate(0," + (height - padding) + ")")
-        .call(d3.axisBottom(xScale));
-
-    // Draw x-axis title
-    svg.append("text")
-        .attr("transform", "translate(" + ((width/2) - padding / 2) + " ," + (height - 10) + ")")
-        .style("text-anchor", "middle")
-        .text("Release Date (Year)");
-
-    // Draw y-axis
-    svg.append("g")
-    	.attr("transform", "translate(" + padding + ", 0)")
-        .call(d3.axisLeft(yScale));
-
-    // Draw y-axis title
-    svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - 5)
-        .attr("x", 0 - (height / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .text("Duration (seconds)");
-
-    drawLegend(colorScale);
-    drawTitle("Duration vs. Release Date");
+  drawLegend(colorScale);
+  drawTitle("Duration vs. Release Date");
 }
 
 /**
@@ -146,68 +230,72 @@ function durationVsReleaseDate(data) {
  * @param {Object} data - a music object
  */
 function durationVsPurchaseDate(data) {
-    // Filter out useless data
-    data = data.filter(filterPurchaseDate)
+  // Filter out useless data
+  data = data.filter(filterPurchaseDate)
 
-    // Update purchase date from string to date object
-    data.forEach(function(d) {
-        d.purchase_date = new Date(d.purchase_date);
+  // Update purchase date from string to date object
+  data.forEach(function(d) {
+    d.purchase_date = new Date(d.purchase_date);
+  });
+
+  // Create x scale
+  var yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) {
+      return timeToSeconds(d.length);
+    })])
+    .range([height - padding, padding]);
+
+  // Create y scale
+  var xScale = d3.scaleTime()
+    .domain(d3.extent(data, getPurchaseDate))
+    .range([padding, width - padding * 2]);
+
+  // Create color scale
+  var colorScale = d3.scaleOrdinal(d3.schemePaired);
+
+  // Draw all circles
+  svg.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cy", function(d) {
+      return yScale(timeToSeconds(d.length));
+    })
+    .attr("cx", function(d) {
+      return xScale(d.purchase_date);
+    })
+    .attr("r", 5)
+    .attr("fill", function(d) {
+      return colorScale(d.genre);
     });
 
-    // Create x scale
-    var yScale = d3.scaleLinear()
-		.domain([0, d3.max(data, function(d) { return timeToSeconds(d.length); })])
-	    .range([height - padding, padding]);
+  // Draw x-axis
+  svg.append("g")
+    .attr("transform", "translate(0," + (height - padding) + ")")
+    .call(d3.axisBottom(xScale));
 
-    // Create y scale
-	var xScale = d3.scaleTime()
-		.domain(d3.extent(data, getPurchaseDate))
-		.range([padding, width - padding * 2]);
+  // Draw x-axis title
+  svg.append("text")
+    .attr("transform", "translate(" + ((width / 2) - padding / 2) + " ," + (height - 10) + ")")
+    .style("text-anchor", "middle")
+    .text("Purchase Date (Year)");
 
-    // Create color scale
-	var colorScale = d3.scaleOrdinal(d3.schemePaired);
+  // Draw y-axis
+  svg.append("g")
+    .attr("transform", "translate(" + padding + ", 0)")
+    .call(d3.axisLeft(yScale));
 
-    // Draw all circles
-    svg.selectAll("circle")
-		.data(data)
-		.enter()
-		.append("circle")
-		.attr("cy", function(d) {
-			return yScale(timeToSeconds(d.length));
-		})
-		.attr("cx", function(d) {
-		    return xScale(d.purchase_date);
-		})
-		.attr("r", 5)
-		.attr("fill", function(d) { return colorScale(d.genre); } );
+  // Draw y-axis title
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - 5)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Duration (seconds)");
 
-    // Draw x-axis
-	svg.append("g")
-	    .attr("transform", "translate(0," + (height - padding) + ")")
-        .call(d3.axisBottom(xScale));
-
-    // Draw x-axis title
-    svg.append("text")
-        .attr("transform", "translate(" + ((width/2) - padding / 2) + " ," + (height - 10) + ")")
-        .style("text-anchor", "middle")
-        .text("Purchase Date (Year)");
-
-    // Draw y-axis
-    svg.append("g")
-    	.attr("transform", "translate(" + padding + ", 0)")
-        .call(d3.axisLeft(yScale));
-
-    // Draw y-axis title
-    svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - 5)
-        .attr("x", 0 - (height / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .text("Duration (seconds)");
-
-    drawLegend(colorScale);
-    drawTitle("Duration vs. Purchase Date");
+  drawLegend(colorScale);
+  drawTitle("Duration vs. Purchase Date");
 }
 
 /**
@@ -222,13 +310,13 @@ function durationVsPurchaseDate(data) {
  * @returns {boolean} true if none of the items listed above
  */
 function filterReleaseDate(data) {
-    var p = data.release_date != null;
-    var g = data.genre != null;
-    var c = data.genre != "Comedy";
-    var u = data.genre != "Country";
-    var s = data.genre != "Soundtrack";
-    var r = data.genre != "Rap";
-    return p && g && c && u && s && r;
+  var p = data.release_date != null;
+  var g = data.genre != null;
+  var c = data.genre != "Comedy";
+  var u = data.genre != "Country";
+  var s = data.genre != "Soundtrack";
+  var r = data.genre != "Rap";
+  return p && g && c && u && s && r;
 }
 
 /**
@@ -243,12 +331,12 @@ function filterReleaseDate(data) {
  * @returns {boolean} true if none of the items listed above
  */
 function filterPurchaseDate(data) {
-    var p = data.purchase_date != null;
-    var g = data.genre != null;
-    var c = data.genre != "Comedy";
-    var u = data.genre != "Country";
-    var s = data.genre != "Soundtrack";
-    return p && g && c && u && s;
+  var p = data.purchase_date != null;
+  var g = data.genre != null;
+  var c = data.genre != "Comedy";
+  var u = data.genre != "Country";
+  var s = data.genre != "Soundtrack";
+  return p && g && c && u && s;
 }
 
 /**
@@ -258,7 +346,7 @@ function filterPurchaseDate(data) {
  * @returns {Date} a purchase date
  */
 function getPurchaseDate(data) {
-    return data.purchase_date;
+  return data.purchase_date;
 }
 
 /**
@@ -268,10 +356,10 @@ function getPurchaseDate(data) {
  * @returns {number} the time string as a number in seconds
  */
 function timeToSeconds(time) {
-    elements = time.split(":");
-    hoursToSeconds = Number(elements[0]) * 3600;
-    minutesToSeconds = Number(elements[1]) * 60;
-    return hoursToSeconds + minutesToSeconds + Number(elements[2]);
+  elements = time.split(":");
+  hoursToSeconds = Number(elements[0]) * 3600;
+  minutesToSeconds = Number(elements[1]) * 60;
+  return hoursToSeconds + minutesToSeconds + Number(elements[2]);
 }
 
 /**
@@ -280,14 +368,14 @@ function timeToSeconds(time) {
  * @param {string} title - the title to be drawn
  */
 function drawTitle(title) {
-    svg.append("text")
-	    .attr("class", "title")
-	    .attr("dy", padding / 2)
-	    .attr("dx", ((width/2) - padding / 2))
-	    .style("text-anchor", "middle")
-	    .style("font-size", "20px")
-        .style("text-decoration", "underline")
-	    .text(title)
+  svg.append("text")
+    .attr("class", "title")
+    .attr("dy", padding / 2)
+    .attr("dx", ((width / 2) - padding / 2))
+    .style("text-anchor", "middle")
+    .style("font-size", "20px")
+    .style("text-decoration", "underline")
+    .text(title)
 }
 
 /**
@@ -296,30 +384,30 @@ function drawTitle(title) {
  * @param {Object} colorScale - a color scale
  */
 function drawLegend(colorScale) {
-    // Create legend
-    var legend = svg.selectAll(".legend")
-        .data(colorScale.domain())
-        .enter().append("g")
-        .attr("class","legend")
-        .attr("transform", function(d,i) {
-            return "translate(0," + i * 20 + ")";
-        });
+  // Create legend
+  var legend = svg.selectAll(".legend")
+    .data(colorScale.domain())
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", function(d, i) {
+      return "translate(0," + i * 20 + ")";
+    });
 
-    // Draw color rectangles on legend
-    legend.append("rect")
-        .attr("x", width - padding)
-        .attr("y", 9)
-        .attr("width",18)
-        .attr("height",18)
-        .style("fill", colorScale);
+  // Draw color rectangles on legend
+  legend.append("rect")
+    .attr("x", width - padding)
+    .attr("y", 9)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", colorScale);
 
-    // Draw legend text
-    legend.append("text")
-        .attr("x", width - padding - 10)
-        .attr("y", 18)
-        .attr("dy",".35em")
-        .style("text-anchor","end")
-        .text(function(d) {
-            return d;
-        });
+  // Draw legend text
+  legend.append("text")
+    .attr("x", width - padding - 10)
+    .attr("y", 18)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text(function(d) {
+      return d;
+    });
 }
